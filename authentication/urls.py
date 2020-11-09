dfb1bb113dee7efff5e1c66a2454e045618b23d5
kpkq <tarasevich_a2000@mail.ru>
2020-11-10 01:51:25 +0300

    added ajax request example for registration form

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import SignUpView
+from .views import SignUpView, validate_form
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
+    path('validate', validate_form, name='validate'),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,10 +1,25 @@
+from django.http import JsonResponse
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
 
 from .forms import CustomUserCreationForm
+from .models import CostControlUser
 
 
 class SignUpView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
-    template_name = 'registration/login.html'
+    template_name = 'registration/registration.html'
+
+
+def validate_form(request):
+    form = CustomUserCreationForm()
+    if request.method == "POST" and request.is_ajax():
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return JsonResponse({"created": True}, status=200)
+        else:
+            return JsonResponse({"created": False}, status=400)
+
+
