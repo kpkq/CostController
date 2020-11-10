d9bd34478de1c3d3d872debee548b9484f6c2332
kpkq <tarasevich_a2000@mail.ru>
2020-11-10 21:54:54 +0300

    added getting spendings

--- control/models.py
+++ control/models.py
@@ -6,7 +6,7 @@ from authentication.models import CostControlUser
 
 class Spendings(models.Model):
     user = models.OneToOneField(CostControlUser, on_delete=models.CASCADE, db_index=True)
-    info = models.JSONField(default='')
+    info = models.JSONField(null=True, blank=True)
 
     def __str__(self):
         return self.user.username
--- control/views.py
+++ control/views.py
@@ -1,18 +1,34 @@
-from django.shortcuts import render
+import json
+
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
+
+from control import models
 from control.forms import SpendingForm
 from control.models import Spendings
 
 
+@login_required
 def get_spendings(request):
     if request.is_ajax():
         form = SpendingForm(request.POST)
         if form.is_valid():
             category = form.cleaned_data.get("category")
             costs = form.cleaned_data.get("costs")
-            user_spendings = Spendings.objects.get_or_create(user=request.user)
-            print()
+            print(category + " " + str(costs))
+            try:
+                spend = Spendings.objects.get(user__email__exact=request.user.email)
+                if category not in spend.info:
+                    spend.info[category] = costs
+                else:
+                    spend.info[category] += costs
+                print(spend.info)
+                spend.save()
+            except Spendings.DoesNotExist:
+                Spendings(user=request.user, info={category: costs}).save()
+
     else:
         form = SpendingForm()
     return render(request, 'home.html', {'form': form})
